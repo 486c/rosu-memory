1d5f3409bdbda0a20eab8bfa40270190f2f00575
486c <lopihavefun@gmail.com>
2023-10-28 15:00:09 +0300

    test: test read_array_T functions properly

--- tests/types_test.rs
+++ tests/types_test.rs
@@ -22,6 +22,72 @@ macro_rules! prim_read_test {
                 let tmp = p.[<read_ $t>](0).unwrap();
                 assert_eq!(tmp, num);
             }
+            
+        }
+    }
+}
+
+macro_rules! prim_read_array_test {
+    ($t: ident) => {
+        paste! {
+            #[test]
+            fn [<test_ $t _array>]() {
+                // Constructing fake array identical how arrays 
+                // stored inside osu!
+                // Type for this example is i32
+                // | 00 00 00 00 | 0C 00 00 00 | 00 00 00 00 |
+                // ^ [1]         ^ [2]         ^ [1]
+                // | 00 00 00 01 | 00 00 00 00 |
+                // ^ [3]         ^ [4]
+                //
+                // [1] - Bytes we don't care about
+                // [2] - Pointer to our data, since we 
+                // are inside test then it's
+                // just a index to our test array
+                // [3] - Size of array
+                // [4] - Array of our desired type
+                // Result of this example gonna be -> [0; 1]
+                let mut rng = rand::thread_rng();
+
+                let mut synthetic_buff = Vec::new();
+                let items_start_idx: i32 = 8;
+                let length: i32 = rng.gen_range(0..1024);
+                let items: Vec<$t> = (0..length)
+                    .map(|_| {
+                        rand::random::<$t>()
+                    }).collect();
+
+                synthetic_buff.extend(
+                    [0u8; 4]
+                ); // Bytes we don't care about
+                synthetic_buff.extend(
+                    items_start_idx.to_le_bytes()
+                ); // Index to array data
+                synthetic_buff.extend(
+                    [0u8; 4]
+                ); // Bytes we don't care about
+                synthetic_buff.extend(
+                    length.to_le_bytes()
+                ); // Size of array
+
+                // Filling our synthetic array with random generated
+                // numbers
+                for item in &items {
+                    synthetic_buff.extend(item.to_le_bytes())
+                }
+
+                let p = FakeProccess {
+                    buff: synthetic_buff
+                };
+
+                let mut fake_output_buff = Vec::new();
+
+                p.[<read_ $t _array>](0, &mut fake_output_buff)
+                    .unwrap();
+
+                assert_eq!(fake_output_buff.len(), length as usize);
+                assert_eq!(fake_output_buff, items);
+            }
         }
     }
 }
@@ -57,7 +123,7 @@ impl ProcessTraits for FakeProccess {
         buff: &mut [u8]
     ) -> Result<(), ProcessError> {
         // Addr - starting index
-        //self.buff.set_position(addr as u64);
+        // self.buff.set_position(addr as u64);
         //self.buff.read(buff);
 
         let mut slice = &self.buff[addr..addr+len];
@@ -139,3 +205,18 @@ prim_read_test!(u128);
 
 prim_read_test!(f32);
 prim_read_test!(f64);
+
+prim_read_array_test!(i8);
+prim_read_array_test!(i16);
+prim_read_array_test!(i32);
+prim_read_array_test!(i64);
+prim_read_array_test!(i128);
+
+prim_read_array_test!(u8);
+prim_read_array_test!(u16);
+prim_read_array_test!(u32);
+prim_read_array_test!(u64);
+prim_read_array_test!(u128);
+
+prim_read_array_test!(f32);
+prim_read_array_test!(f64);
